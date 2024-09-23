ecdadb5ab23730962ac6ba29304873276a2c0483
samim <samimsaleklotfi@gmail.com>
2024-09-23 00:29:43 -0800

    the fifth ine is done

--- assignment13.js
+++ assignment13.js
@@ -51,6 +51,20 @@ function isPalindrome(str) {
 }
 // also i write this code bu using chat-gpt
 
+// . Write a function to find the maximum number in an array.
+function findMax(arr) {
+    if (arr.length === 0) {
+        return null; 
+    }
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
 
 
 
